25fedbb9b42be56ea004b753e3867b39f86e8d0e
richard <brayanrichard.vargas@alumnos.ulagos.cl>
2024-09-10 09:13:06 -0300

    primera version 09-09-2024

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 
-import React from 'react';
+
 import Header from './components/Header/Header.jsx';
 import './App.css';
 import Footer from "./components/Footer/Footer.jsx";
@@ -14,7 +14,7 @@ function App() {
                     <div className="main-content">
 
                         <h1 style={{color: 'yellowgreen'}}>introduccion a MOUNTAIN HEAT</h1>
-                        <p style={{color: 'lightblue'}}>en este apartado conoceras sobre este sitio web dedicado al equipamiento cordillerano</p>
+                        <p style={{color: 'lightblue'}}>en este apartado conoceras sobre este sitio web dedicado al equipamiento cordillerano, encuntra lo que buscas aqui.</p>
                     </div>
                     <Footer/>
                 </div>
